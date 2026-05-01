53830e98dd23a663a0272b96c2ac8f5523d3db25
genagithub <143031972+genagithub@users.noreply.github.com>
2026-05-01 01:01:58 -0300

    commit

--- app.py
+++ app.py
@@ -57,8 +57,8 @@ df_pca["Order Success"] = df["Order Success"].values
 success = df_pca.loc[df_pca["Order Success"] == 1,:]
 fails = df_pca.loc[df_pca["Order Success"] == 0,:]
 
-df_value_counts = df["Order Success"].value_counts(normalize=True)
-success_prc, fails_prc = df_value_counts.loc[1], df_value_counts.loc[0]
+df_value_counts = df_original["Order Success"].value_counts(normalize=True)
+success_prc, fails_prc = df_value_counts.loc[1]*100, df_value_counts.loc[0]*100
 
 probability_text = html.B(id="probability", children=[], style={})
 
