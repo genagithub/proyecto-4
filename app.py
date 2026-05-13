d9cb1b19dcaa71fe78beadd52a5e799a372442a6
genagithub <143031972+genagithub@users.noreply.github.com>
2026-05-12 21:19:40 -0300

    commit

--- app.py
+++ app.py
@@ -132,7 +132,8 @@ def get_risk_prob(n_clicks, var_1, var_2, var_3, var_4, var_5, var_6, var_7):
             df_cat = pd.DataFrame(obj_cat_enc, columns=categorical_vars)
             object_to_predict = pd.concat([df_num, df_cat], axis=1)[X_train_columns]
 
-            prob_fail = bagging_knn.predict_proba(object_to_predict)[0, 0] * 100
+            probs = bagging_knn.predict_proba(object_to_predict)
+            prob_fail = probs[0][0] * 100 
             prob_fail_text = f"{prob_fail:.2f}"
             color_res = "red" if prob_fail > 45 else "green"
             style_res = {"color": color_res}
