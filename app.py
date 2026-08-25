bf8f0763f52190aa43a629053f18d629aa8191f0
genagithub <143031972+genagithub@users.noreply.github.com>
2026-08-25 17:55:38 -0300

    commit

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from dash import html, dcc
 from dash.dependencies import Output, Input, State
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 from sklearn.decomposition import PCA
-from prince import FAMD
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import BaggingClassifier
 
@@ -26,47 +25,50 @@ df = df.reset_index(drop=True)
 categorical_vars = ["Category Name", "Market", "Order Region", "Shipping Mode"]
 numeric_vars = ["Days for shipment (scheduled)", "Product Price", "Discount Ratio"]
 
-for var in categorical_vars:
-    df[var] = df[var].astype("category")
+X_train, X_test, y_train, y_test = train_test_split(df[categorical_vars + numeric_vars],
+                                                    df["Order Success"],
+                                                    test_size=0.25,
+                                                    random_state=42,
+                                                    stratify=df["Order Success"])
 
-for var in numeric_vars:
-    df[var] = pd.to_numeric(df[var])
+smote = SMOTE(sampling_strategy=0.176, random_state=42)
+X_train, y_train = smote.fit_resample(X_train, y_train)
 
-X_columns = categorical_vars + numeric_vars
-X_train_data = df[X_columns]
-y_train = df["Order Success"]
+scaler = StandardScaler()
+X_train[numeric_vars] = scaler.fit_transform(X_train[numeric_vars])
 
-famd = FAMD(n_components=2, n_iter=5, random_state=42)
-famd_results = famd.fit_transform(X_train_data)
+encoder = OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1)
+encoder.fit(X_train[categorical_vars])
+X_train[categorical_vars] = encoder.transform(X_train[categorical_vars])
 
 knn_classifier = KNeighborsClassifier(n_neighbors=5)
 
 bagging_knn = BaggingClassifier(
     estimator=knn_classifier,
-    n_estimators=20,
+    n_estimators=100,
     max_samples=0.3,
     bootstrap=True,
     n_jobs=1  
 )
 
-bagging_knn.fit(famd_results, y_train)
+bagging_knn.fit(X_train, y_train)
 
-df_famd = pd.DataFrame(famd_results.values, columns=["FAMD1", "FAMD2"])
-df_famd["Order Success"] = y_train.values
+pca = PCA(n_components=2)
+pca_results = pca.fit_transform(X_train_data)
 
-success = df_famd.loc[df_famd["Order Success"] == 1, :]
-fails = df_famd.loc[df_famd["Order Success"] == 0, :]
+df_pca = pd.DataFrame(pca_results, columns=["PC1", "PC2"])
+df_pca["Order Success"] = df["Order Success"].values
 
-df_value_counts = df_original["Order Success"].value_counts(normalize=True)
-success_prc, fails_prc = round(df_value_counts.loc[1]*100,1), round(df_value_counts.loc[0]*100,1)
+success = df_pca.loc[df_pca["Order Success"] == 1,:]
+fails = df_pca.loc[df_pca["Order Success"] == 0,:]
 
-probability_text = html.B(id="probability", children=[], style={})
+probability_text = html.B(id="probability",children=[],style={})
 
-fig_famd = go.Figure()
-fig_famd.add_trace(go.Scatter(x=success["FAMD1"], y=success["FAMD2"], mode="markers", marker_color="green", name=f"Completadas ({success_prc}%)"))
-fig_famd.add_trace(go.Scatter(x=fails["FAMD1"], y=fails["FAMD2"], mode="markers", marker_color="red", name=f"Sin éxito ({fails_prc}%)"))
-fig_famd.update_layout(title="Resultados de órdenes históricas")
-fig_famd.update_layout(legend=dict(font=dict(size=9)))
+fig_pca = go.Figure()
+fig_pca.add_trace(go.Scatter(x=success["PC1"], y=success["PC2"], mode="markers", marker_color="green", name="Completadas"))
+fig_pca.add_trace(go.Scatter(x=fails["PC1"], y=fails["PC2"], mode="markers", marker_color="red", name="Sin éxito"))
+fig_pca.update_layout(title="Resultados de órdenes históricas")
+fig_pca.update_layout(legend=dict(font=dict(size=9)))
 
 app = dash.Dash(__name__)
 server = app.server
@@ -108,60 +110,56 @@ app.layout =  html.Div(id="body",className="e4_body",children=[
 
 def get_risk_prob(n_clicks, var_1, var_2, var_3, var_4, var_5, var_6, var_7):
   
-    fig_update = go.Figure(fig_famd)
+    fig_update = go.Figure(fig_pca)
     prob_fail_text = "0.00%"
     style_res = {"color": "black"}
 
     inputs = [var_1, var_2, var_3, var_4, var_5, var_6, var_7]
     if n_clicks > 0 and all(v is not None for v in inputs):
-        try:
-            new_object = pd.DataFrame({
-                "Days for shipment (scheduled)": [float(var_1)],
-                "Market": [str(var_2)],
-                "Order Region": [str(var_3)],
-                "Category Name": [str(var_4)],
-                "Product Price": [float(var_5)],
-                "Discount Ratio": [float(var_6)],
-                "Shipping Mode": [str(var_7)]
-            })
+        new_object = pd.DataFrame({
+            "Days for shipment (scheduled)": [float(var_1)],
+            "Market": [str(var_2)],
+            "Order Region": [str(var_3)],
+            "Category Name": [str(var_4)],
+            "Product Price": [float(var_5)],
+            "Discount Ratio": [float(var_6)],
+            "Shipping Mode": [str(var_7)]
+        })
 
-            for var in categorical_vars:
-                new_object[var] = pd.Categorical(new_object[var], categories=df[var].cat.categories)
+        obj_num_scaled = scaler.transform(new_object[numeric_vars])
+        obj_cat_enc = encoder.transform(new_object[categorical_vars])
 
-            for var in numeric_vars:
-                new_object[var] = pd.to_numeric(new_object[var])
+        df_num = pd.DataFrame(obj_num_scaled, columns=numeric_vars)
+        df_cat = pd.DataFrame(obj_cat_enc, columns=categorical_vars)
+        object_to_predict = pd.concat([df_cat, df_num], axis=1)[categorical_vars + numeric_vars]]
 
-            object_to_predict = famd.transform(new_object[X_columns])
+        prob_fail = bagging_knn.predict_proba(object_to_predict)[0, 0] * 100
+        prob_fail_text = f"{prob_fail:.2f}"
 
-            prob_fail = bagging_knn.predict_proba(object_to_predict)[0,0] * 100 
-            prob_fail_text = f"{prob_fail:.2f}%"
-
-            if prob_fail <= 45:
-                factor = prob_fail / 45
-                hue = int(120 - (factor * 120))
-                color_res = f"hsl({hue}, 100%, 45%)"
-            elif 45 < prob_fail <= 50:
-                color_res = "hsl(0, 100%, 45%)"
-            else:
-                dark_factor = (prob_fail - 50) / 50
-                lightness = int(45 - (dark_factor * 25)) 
-                color_res = f"hsl(0, 100%, {lightness}%)"
+        if prob_fail <= 45:
+            factor = prob_fail / 45
+            hue = int(120 - (factor * 120))
+            color_res = f"hsl({hue}, 100%, 45%)"
+        elif 45 < prob_fail <= 50:
+            color_res = "hsl(0, 100%, 45%)"
+        else:
+            dark_factor = (prob_fail - 50) / 50
+            lightness = int(45 - (dark_factor * 25))
+            color_res = f"hsl(0, 100%, {lightness}%)"
 
             style_res = {"color":color_res}
 
-            fig_update.add_trace(go.Scatter(
-                x=[object_to_predict.iloc[0, 0]], 
-                y=[object_to_predict.iloc[0, 1]], 
-                mode="markers", 
-                marker=dict(color="blueviolet", size=16, symbol="star", line=dict(width=1, color="white")), 
-                name="Nueva orden"
-            ))
-            
-        except Exception as e:
-            print(f"Error en el cálculo: {e}")
-            prob_fail_text = "ERROR"
+        obj_pca = pca.transform(object_to_predict)
 
-    return fig_update, prob_fail_text, style_res
+        fig_update.add_trace(go.Scatter(
+            x=[obj_pca[0, 0]],
+            y=[obj_pca[0, 1]],
+            mode="markers",
+            marker=dict(color="blueviolet", size=15, symbol="star"),
+            name="Nuevo producto"
+        ))
+
+    return fig_pca, prob_fail_text, style_res
 
 
 if __name__ == "__main__":
