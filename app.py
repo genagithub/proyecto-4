2301a81f7233f2caafc2a7907eea516a3e252b8e
genagithub <143031972+genagithub@users.noreply.github.com>
2026-08-26 00:19:35 -0300

    commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import dash
 from dash import html, dcc
 from dash.dependencies import Output, Input, State
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder, StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import BaggingClassifier
