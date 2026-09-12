de36601a36e87d989cf167c1989747b0a20da656
genagithub <143031972+genagithub@users.noreply.github.com>
2026-09-12 20:17:17 -0300

    commit

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ app.layout =  html.Div(id="body",className="e4_body",children=[
                       data=[], 
                       fill_width=True,
                       style_table={"width":"100%","minWidth":"100%"},
-                      style_cell={"textAlign":"left","padding":"8px","fontFamily":"Arial"}, 
+                      style_cell={"textAlign":"left","padding":"5px","fontFamily":"Arial","fontSize":"12px","whiteSpace":"normal","height":"auto"}, 
                       style_header={"backgroundColor":"#f4f4f4","fontWeight":"bold"})])
         ]),
         html.Div(id="input_div", style={"display":"flex","flexWrap":"wrap","gap":"10px"}, children=[
