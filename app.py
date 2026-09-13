e3ae1ee3cf0613c9ff1d274ce56758d8b8c177f8
genagithub <143031972+genagithub@users.noreply.github.com>
2026-09-12 21:27:33 -0300

    commit

--- app.py
+++ app.py
@@ -202,8 +202,8 @@ def get_risk_prob(n_clicks, var_1, var_2, var_3, var_4, var_5, var_6, var_7, tab
         style_res = {"color":color_res}      
       
         fig_update.add_trace(go.Scatter(
-            x=[obj_pca[0, 0]],
-            y=[obj_pca[0, 1]],
+            x=obj_pca[0, 0],
+            y=obj_pca[0, 1],
             mode="markers",
             marker=dict(color="blueviolet", size=12, symbol="star"),
             name=f"Nueva Operación {n_clicks}"
